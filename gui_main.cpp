6ecad3116e6b683d1e8964cf760b0e1343b9484a
Farzeen <happycoder97@protonmail.com>
2019-01-31 12:01:14 +0530

    function prototypes

--- gui_main.cpp
+++ gui_main.cpp
@@ -4,6 +4,13 @@ void gui_setup(int argc, char** argv) {
 
 }
 
+
+void signin();
+void assignments();
+void questions();
+void testcases();
+
+
 void gui_loop() {
     ImGui::Begin("Test");
     ImGui::Text("Hello World!");
